e536c505d852f27d22573cb385f78626ca6d7de4
Breno RdV <noreply@raccoon.ninja>
2023-12-16 02:48:21 -0500

    fixed bug on the sys_mon utils

--- src/sys_mon/sys_mon_utils.py
+++ src/sys_mon/sys_mon_utils.py
@@ -15,6 +15,7 @@ _last_sent_storage_total = None
 _last_sent_nas_total = None
 _current_time = None
 _net_io_last = None
+_current_server_status = None
 
 
 def _get_disk_usage(path):
@@ -29,59 +30,62 @@ def get_system_status():
     global _last_sent_nas_total
     global _current_time
     global _net_io_last
+    global _current_server_status
     _logger.debug("Getting system status")
 
     try:
-        server_info = SysStats()
+        if _current_server_status is None:
+            _current_server_status = SysStats()
+
         _net_io_last = psutil.net_io_counters()
         time.sleep(5)  # Wait a bit to let the network usage "settle"
 
         _current_time = time.time()
 
         _logger.debug("Setting CPU usage")
-        server_info.update_cpu_percent(psutil.cpu_percent())
+        _current_server_status.update_cpu_percent(psutil.cpu_percent())
 
         mem = psutil.virtual_memory()
         _logger.debug("Setting memory usage")
-        server_info.update_mem_used(mem.used)
+        _current_server_status.update_mem_used(mem.used)
 
         _logger.debug("Getting network usage")
         net_io_current = psutil.net_io_counters()
 
         _logger.debug("Setting network usage - upload")
         upload_rate = net_io_current.bytes_sent - _net_io_last.bytes_sent
-        server_info.update_net_upload(upload_rate)
+        _current_server_status.update_net_upload(upload_rate)
 
         _logger.debug("Setting network usage - download")
         download_rate = net_io_current.bytes_recv - _net_io_last.bytes_recv
-        server_info.update_net_download(download_rate)
+        _current_server_status.update_net_download(download_rate)
 
         _net_io_last = net_io_current
 
         # Update data every 10 minutes
         if _last_sent_storage_total is None or _current_time - _last_sent_storage_total >= 600:
             _logger.debug("Setting local storage usage")
-            server_info.update_storage_used(_get_disk_usage(LOCAL_DISK_PATH)[0])
+            _current_server_status.update_storage_used(_get_disk_usage(LOCAL_DISK_PATH)[0])
 
             _logger.debug("Setting NAS usage")
-            server_info.update_nas_used(_get_disk_usage(NAS_PATH)[0])
+            _current_server_status.update_nas_used(_get_disk_usage(NAS_PATH)[0])
 
             _last_sent_storage_total = _current_time
 
         # Update data every 24 hours
         if _last_sent_nas_total is None or _current_time - _last_sent_nas_total >= 86400:
             _logger.debug("Setting total local storage")
-            server_info.update_storage_total(_get_disk_usage(LOCAL_DISK_PATH)[1])
+            _current_server_status.update_storage_total(_get_disk_usage(LOCAL_DISK_PATH)[1])
 
             _logger.debug("Setting total NAS")
-            server_info.update_nas_total(_get_disk_usage(NAS_PATH)[1])
+            _current_server_status.update_nas_total(_get_disk_usage(NAS_PATH)[1])
 
             _logger.debug("Setting total memory")
-            server_info.update_mem_total(mem.total)
+            _current_server_status.update_mem_total(mem.total)
 
             _last_sent_nas_total = _current_time
 
-        return server_info.to_dict()
+        return _current_server_status.to_dict()
 
     except Exception as e:
         _logger.exception(f"Error while getting system status: {e}")
